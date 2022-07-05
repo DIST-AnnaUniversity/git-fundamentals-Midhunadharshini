56195b205d64cbd2230e0b915579e061f4c632d8
Midhunadharshini <108231899+Midhunadharshini@users.noreply.github.com>
2022-07-05 17:12:58 +0530

    Add files via upload

---
+++ matrix operation.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install numpy
+
+
+# In[2]:
+
+
+#Matrix Addition
+import numpy as np
+a=np.array([[1,2,3],[4,7,3],[2,6,3]])
+b=np.array([[2,4,8],[3,3,7],[4,7,5]])
+mat_sum=a+b
+print ("Matrix Multiplication is :",mat_sum)
+
+
+# In[3]:
+
+
+#Matrix Subtraction
+import numpy as np
+a=np.array([[1,2,3],[4,7,3],[2,6,3]])
+b=np.array([[2,4,8],[3,3,7],[4,7,5]])
+mat_sub=a-b
+print ("Matrix Subtraction is :",mat_sub)
+
+
+# In[4]:
+
+
+#Matrix Multiply
+import numpy as np
+a=np.array([[1,2,3],[4,7,3],[2,6,3]])
+b=np.array([[2,4,8],[3,3,7],[4,7,5]])
+mat_mul=a*b
+print ("Matrix Multiplication is :",mat_mul)
+
+
+# In[ ]:
+
+
+
+
